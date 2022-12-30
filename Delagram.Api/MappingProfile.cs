afe4e59ca4c17aa8b4c215e2702de46a999a2d7b
bruno-koster <bruno.koster@umbler.com>
2022-12-30 18:07:17 -0300

    Dale

---
+++ Delagram.Api/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Delegram.Api.Models;
+using Delegram.Api.Models.DTOs;
+
+namespace Delegram.Api
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, LoginDTO>();
+        }
+    }
+}
